0f0b6fb4d5209c2834b1552729e0740314c8bf00
leonardo <a20201696@pucp.edu.pe>
2023-06-20 21:01:45 -0500

    Avances en colores y selecciones

--- src/main/resources/static/Selecccion.js
+++ src/main/resources/static/Selecccion.js
@@ -2,84 +2,31 @@
     $(document).ready(function () {
     $("#seleccion").click(function (event) {
         event.preventDefault();
-        var personaje = $("#i");
+        var personaje = $("#i").val();
 console.log(personaje)
         if(personaje=="Bulbasur") {
             alert("123");
                 $("body").append('<p>a</p>');
-                var div = document.getElementById("color");
-                div.style.backgroundColor = {
-                    rgba: {
-                        props: {
-                            green: {
-                                idx: 1,
-                                type: "byte"
-                            }
-                        }
-                    },
-                }
-            return;
         }
         if (personaje=="Chansey") {
 
                 $("body").append('<p>b</p>');
-            var div = document.getElementById("color");
-            div.style.backgroundColor = {
-                rgba: {
-                    props: {
-                        rose: {
-                            idx: 1,
-                            type: "byte"
-                        }
-                    }
-                },
-            }
             return;
         }
         if (personaje=="Charmander") {
             alert("123");
                 $("body").append('<p>c</p>');
-            var div = document.getElementById("color");
-            div.style.backgroundColor = {
-                rgba: {
-                    props: {
-                        oranje: {
-                            idx: 1,
-                            type: "byte"
-                        }
-                    }
-                },
-            }
+
             return;
         }
         if (personaje=="Cubone") {
                 $("body").append('<p>d</p>');
-            var div = document.getElementById("color");
-            div.style.backgroundColor = {
-                rgba: {
-                    props: {
-                        white: {
-                            idx: 1,
-                            type: "byte"
-                        }
-                    }
-                },
-            }
+
             return;
         }
         if (personaje=="Squirtle") {
                 $("body").append('<p>e</p>');
-            var div = document.getElementById("color");
-            div.style.backgroundColor = {
-                rgba: {
-                    props: {
-                        blue: {
-                            idx: 1,
-                            type: "byte"
-                        }
-                    }
-                },
-            }
+
             return;
         }
         else {
@@ -89,3 +36,9 @@ console.log(personaje)
     });
 
 });
+    function cambiarColor() {
+        var colorPicker = document.getElementById("colorPicker");
+        var div = document.getElementById("change");
+        var color = colorPicker.value;
+        div.style.backgroundColor = color;
+    }
