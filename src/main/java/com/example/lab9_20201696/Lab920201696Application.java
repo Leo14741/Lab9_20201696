dbc6c034be4d4faed495ac7f78bfe3c6c2ee88e2
leonardo <a20201696@pucp.edu.pe>
2023-06-20 19:12:52 -0500

    Initial commit

---
+++ src/main/java/com/example/lab9_20201696/Lab920201696Application.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20201696;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab920201696Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab920201696Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab9_20201696/Lab920201696ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20201696;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab920201696ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
